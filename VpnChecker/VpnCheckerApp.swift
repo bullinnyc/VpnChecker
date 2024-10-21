477d531c9eaca3a64ba7a8a0d286cd3409d42f41
bullinnyc <i8u11@icloud.com>
2024-10-21 08:01:40 +0700

    Initial Commit

---
+++ VpnChecker/VpnCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VpnCheckerApp.swift
+//  VpnChecker
+//
+//  Created by Dmitry Kononchuk on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VpnCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
